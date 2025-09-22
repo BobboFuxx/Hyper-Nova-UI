ad1cf71eb08c7574c4cac0a2cc1c61f9a3428b30
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2025-09-22 12:28:34 -0700

    Create bridge.tsx

---
+++ pages/bridge.tsx
@@ -0,0 +1,10 @@
+import BridgeForm from "../components/BridgeForm";
+
+export default function BridgePage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Hyper-Nova IBC Bridge</h1>
+      <BridgeForm />
+    </div>
+  );
+}
